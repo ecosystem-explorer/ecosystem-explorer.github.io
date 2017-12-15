a4ef47db508382be37d785b034dc8312059f5c71
Christoph Reimers <christoph@superservice-international.com>
2017-12-15 11:36:29 +0100

    bg image improvements

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from './components/Footer'
 import './App.css';
 
 import backgroundImageUrl from './eco-bg.jpg'
+import backgroundImageBlurUrl from './eco-bg-blur.jpg'
 
 const AppWrapper = styled.div`
   width: 100vw;
@@ -21,13 +22,31 @@ const AppWrapper = styled.div`
     content: "";
     background: url(${backgroundImageUrl});
     background-size: cover;
-    opacity: ${props => props.bgImgSrc !== '' ? .7 : 0};
+    opacity: ${props => props.showBg ? .7 : 0};
     position: absolute;
     z-index: -1;
     top: 0;
     left: 0;
     height: 100vh;
     width: 100vw;
+    transition: all 1s ease;
+    overflow: hidden;
+    transform: scale(1.1);
+  }
+  &:before {
+    content: "";
+    background: url(${backgroundImageBlurUrl});
+    z-index: -2;
+    background-size: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: ${props => props.showBg ? 0 : 0.7};
+    height: 100vh;
+    width: 100vw;
+    filter: blur(15px);
+    overflow: hidden;
+    transform: scale(1.1);
     transition: all 1s ease;
   }
 `
@@ -36,15 +55,17 @@ const AppWrapper = styled.div`
 class App extends Component {
 
   state = {
-    bgImgSrc: ''
+    showBg: false
   }
 
   componentDidMount() {
     const backgroundImage = new Image()
     backgroundImage.onload = () => {
-      this.setState({
-        bgImgSrc: backgroundImage.src
-      })
+      setTimeout(() => {
+        this.setState({
+          showBg: true
+        })
+      }, 2000)
     }
     backgroundImage.src = backgroundImageUrl
   }
@@ -52,7 +73,7 @@ class App extends Component {
   render() {
 
     return (
-      <AppWrapper bgImgSrc={this.state.bgImgSrc}>
+      <AppWrapper showBg={this.state.showBg}>
         <Header />
         <Main />
         <Footer />
